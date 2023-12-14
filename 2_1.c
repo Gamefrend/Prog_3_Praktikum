7494d744ae8864d4370f8a56def6074736e7522b
Enis <eni.avdovic@live.de>
2023-12-14 16:15:58 +0100

    CDatein Verschoben und Unterordner für C und Python erstellt

--- 2_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void){
-    int a;
-    int b;
-    scanf("%d",&a);
-    scanf("%d",&b);
-    printf("%d",a);
-    printf("%d",b);
-}
---
+++ cFiles/2_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+    int a;
+    int b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("%d",a);
+    printf("%d",b);
+}
