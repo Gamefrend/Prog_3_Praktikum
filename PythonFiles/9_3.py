def573054a75e39c2de460d2116d9285c9d9d2c8
Enis <eni.avdovic@live.de>
2024-01-13 15:23:38 +0100

    9_3 fertig

--- PythonFiles/9_3.py
+++ PythonFiles/9_3.py
@@ -13,11 +13,24 @@ def listToString(list):
         out+=e
     return out
 
-countedChars = {"a":0}
+countedCharsDict = {"a":0}
 def countChars(x):
-    if x not in countedChars:
-        countedChars.update({x: 1})
+    x = x.lower()
+    if x not in countedCharsDict:
+        if not x.isalpha():
+            return
+        countedCharsDict.update({x: 1})
     else:
-        countedChars.update({x: countedChars.get(x)+1})
+        countedCharsDict.update({x: countedCharsDict.get(x)+1})
 
-print(list(map(countChars,text)))
+def mapChartoList(x):
+    if x in sortedList:
+        return haefigkeitListe[sortedList.index(x)]
+    else:
+        return x
+
+haefigkeitListe = ['e', 'n', 'i', 'a', 's', 't', 'r', 'u', 'h', 'd', 'l', 'c', 'm', 'o', 'g', 'k', 'w', 'b', 'z', 'f', 'v', 'p', 'j', 'x', 'y', 'q']
+
+list(map(countChars,text))
+sortedList = sorted(countedCharsDict, key=countedCharsDict.get, reverse=True)
+print(listToString(list(map(mapChartoList, text))))
