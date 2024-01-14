c3186a8d4d1a9bac64126e51e03b4f15e48215e3
Enis <eni.avdovic@live.de>
2024-01-14 13:43:29 +0100

    9_5 fertig

--- PythonFiles/9_5.py
+++ PythonFiles/9_5.py
@@ -1,3 +1,7 @@
+import glob
 import os.path
 
 print(os.path.getsize(r"C:\Users\eniav\Desktop\Uni Aufgaben\Prog 3\bonz.txt"))
+listOfFilenames = glob.glob("**/*.py", recursive=True)
+print(list(map(lambda x:x[0], sorted([(n, os.path.getsize(n)) for n in listOfFilenames],key= lambda x:x[1])[:-4:-1])))
+
