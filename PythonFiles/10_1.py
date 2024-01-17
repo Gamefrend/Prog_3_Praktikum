9acf418747bd508e8ddab9c63d20f138ef0119ac
Enis <eni.avdovic@live.de>
2024-01-17 15:07:35 +0100

    Blatt 10_1 fertig

--- PythonFiles/10_1.py
+++ PythonFiles/10_1.py
@@ -11,16 +11,24 @@ class Messungen:
             self.temperatura = float(elements[0].split(",")[1])
 
     def __repr__(self):
-        return f"Diese Messung hat den Timestamp {self.timeStamp} und Temperatur von {self.temperatura}"
+        return f"Messungen('{self.timeStamp}', {self.temperatura})"
 
     def __eq__(self, other):
-        print("??")
-        return self.timeStamp == other.timeStamp and self.temperatura == other.temperatura
+        return str(self.timeStamp) == str(other.timeStamp) and self.temperatura == other.temperatura
 
+    def __gt__(self, other):
+        return self.temperatura > other.temperatura
 
-Messungen1 = Messungen("2019-01-11 17:45:01.356640", 19.5)
-Messungen2 = Messungen('"2019-01-11 17:45:01.356640",19.5')
+    def __lt__(self, other):
+        return self.temperatura <= other.temperatura
 
-print(Messungen2)
-eval(repr(Messungen1)) == Messungen1
+    def __str__(self):
+        return f'Der Timstamp liegt bei {self.timeStamp} und die Temparatur bei {self.temperatura}'
 
+messungen1 = Messungen("2019-01-11 17:45:01.356640", 21.5)
+messungen2 = Messungen('"2019-01-11 17:45:01.356640",20.5')
+messungen3 = Messungen("2019-01-11 17:45:01.356640", -20)
+alleMessungen = [messungen1, messungen2, messungen3]
+print(messungen1)
+print(messungen1 == messungen2)
+print(sorted(alleMessungen))
