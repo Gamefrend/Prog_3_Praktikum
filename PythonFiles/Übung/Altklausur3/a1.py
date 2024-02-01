510d9eb3d16e022ef0cc97244fc479f2ddad5fb6
Enis <eni.avdovic@live.de>
2024-02-02 00:20:44 +0100

    Noch weiter mit Altklaurusen gelernt

--- PythonFiles/Übung/Altklausur3/a1.py
+++ PythonFiles/Übung/Altklausur3/a1.py
@@ -1,23 +1,23 @@
+import re
 class RoemerZahl:
     def __init__(self, input=""):
+        if re.compile("^([C]*[L]*[X]*[V]*[I]*)$|([1-9]+)").fullmatch(input) is None and not input == "":
+            raise ValueError("")
+        print(re.compile("^([C]*[L]*[X]*[V]*[I]*)*$|([1-9]+)").fullmatch(input))
         self.roemisch = input
         self.modern = self.umwandeln(input)
 
     def umwandeln(self, input):
         output = 0
         lookup = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100}
-        counter = 0
-        while counter < len(input):
-            if counter+1 <len(input):
-                if input[counter] < input[counter+1]:
-                    output += lookup[input[counter+1]] - lookup[input[counter]]
-                    counter += 1
-                else:
-                    output += lookup[input[counter]]
-            else:
-                output += lookup[input[counter]]
-            counter += 1
+        for char in input:
+            output += lookup[char]
         return output
+    def __repr__(self):
+        return "RoemerZahl('"+self.roemisch+"')"
+
+    def __str__(self):
+        return self.roemisch
 
 zahl1 = RoemerZahl("")
 zahl2 = RoemerZahl("XIII")
@@ -25,3 +25,5 @@ zahl3 = RoemerZahl("C")
 zahl4 = RoemerZahl("IXIIV")
 print(zahl2.modern)
 print(zahl4.modern)
+print(str(eval(repr(zahl2))))
+print(str(zahl1) == "")
